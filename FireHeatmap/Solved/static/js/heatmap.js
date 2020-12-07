9ad370673d41060095cc719aa687d053b4baa443
sharmila <sharrao08@gmail.com>
2020-12-07 15:36:01 -0500

    init

--- FireHeatmap/Solved/static/js/heatmap.js
+++ FireHeatmap/Solved/static/js/heatmap.js
@@ -1,6 +1,6 @@
 var myMap = L.map("map", {
-  center: [37.7749, -122.4194],
-  zoom: 13
+  center: [39.1432892,-96.8211714,4.92], //[37.7749, -122.4194],
+  zoom: 5
 });
 
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -12,25 +12,58 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-var link = "static/csv/data.csv";
 
-d3.csv(link, function(response) {
+var link = "static/csv/data.csv";
+//var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000";
+
+//d3.json(url, function(response) {
+ d3.csv(link, function(response) {
 
   console.log(response);
 
   var heatArray = [];
 
   for (var i = 0; i < response.length; i++) {
-    var location = response[i].location;
+    var location = response[i];
 
     if (location) {
-      heatArray.push([location.coordinates[1], location.coordinates[0]]);
+      heatArray.push([location.lat, location.lon]);
+      //console.log(heatArray[heatArray.length - 1]);
     }
   }
 
   var heat = L.heatLayer(heatArray, {
+    maxZoom: 3,
+    //minZoom:10,
     radius: 20,
-    blur: 35
+    max: 2,
+    blur: 6,
+    gradient: {
+      0.0:'blue',
+      0.42: 'crimson',
+      0.65: 'lime',
+      1.0: 'red'
+    }
   }).addTo(myMap);
 
+
 });
+function circleStyler (location) {
+  var circleStyle = {};
+  console.log("ff");
+  console.log(location);
+  var circleRadius = location.acres;
+  var circleColor;
+  var circleFillColor;
+  if (location.cause === "Lightning") {
+    circleColor = "blue"
+    circleFillColor = "blue"
+  } else if (location.cause === "Unknown") {
+    circleColor = "gray"
+    circleFillColor = "gray"
+  }
+  circleStyle = {radius: circleRadius,
+                color: circleColor,
+                fillColor: circleFillColor}
+  return circleStyle
+}
